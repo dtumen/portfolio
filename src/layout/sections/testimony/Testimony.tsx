7145bde8bbbe824f69e2f93d591768e0a3d4e5d5
Tumen Dambaev <dt1109@mail.ru>
2024-01-29 23:32:28 +0300

    add Testimony
    Добавил:
    1) Секцию Testimony
    2) Компонент Slider

---
+++ src/layout/sections/testimony/Testimony.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from 'styled-components'
+import { SectionTitle } from '../../../components/SectionTitle'
+import Icon from '../../../components/icon/Icon'
+import Slider from '../../../components/Slider'
+import { FlexWrapper } from '../../../components/FlexWrapper'
+
+export default function Testimony() {
+  return (
+    <StyledTestimony>
+      <SectionTitle>Testimony</SectionTitle>
+      <FlexWrapper align='center' direction='column'>
+        <Icon iconId={'quote'} />
+        <Slider />
+      </FlexWrapper>
+    </StyledTestimony>
+  )
+}
+
+
+const StyledTestimony = styled.section`
+  background-color: #aeb9ff;
+  min-height: 50vh;
+`
