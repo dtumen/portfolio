aa27e390af8f48374823d2e6028a2880387928e5
Tumen Dambaev <dt1109@mail.ru>
2024-02-14 16:04:10 +0300

    add useState in header

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -22,7 +22,7 @@ export const Footer: React.FC = () => {
           }
           
         </S.SocialList>
-        <S.Copyright>© 2023 Svetlana Dyablo, All Rights Reserved.</S.Copyright>
+        <S.Copyright>© 2023 Tumen Dambaev, All Rights Reserved.</S.Copyright>
       </FlexWrapper>
     </S.Footer>
   )
