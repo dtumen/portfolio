13972368a865ddd070ea38bd01d561fcc49f1c25
Tumen Dambaev <dt1109@mail.ru>
2024-02-15 12:51:22 +0300

    add react-awesome-reveal

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -5,6 +5,7 @@ import { FlexWrapper } from '../../../components/FlexWrapper';
 import { SectionTitle } from '../../../components/SectionTitle';
 import { Container } from '../../../components/Container';
 import { Skill } from './skill/Skill';
+import { Fade } from "react-awesome-reveal";
 
 import { S } from './Skills_Styles'
 
@@ -18,9 +19,11 @@ export const Skills: React.FC = () => {
       <Container>
         <SectionTitle>My Skills</SectionTitle>
         <FlexWrapper wrap='wrap' justify='space-between'>
+          <Fade cascade={true} damping={.2}>
           {skillData && skillData.map((el, i) => (
               <Skill iconId={el.iconId} title={el.title} description={el.description} key={i}/>
           ))}
+          </Fade>
         </FlexWrapper>
       </Container>
     </S.Skills>
